74dc4a348f18d651024e2bfb40db3ba884b2007b
PeterPain <yannik.schroeder@gmail.com>
2020-11-22 18:06:41 +0100

    added chessalyzer base

--- app/containers/ChessboardPage/ChessboardPage.tsx
+++ app/containers/ChessboardPage/ChessboardPage.tsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import { ipcRenderer } from 'electron';
 
 export default function ChessboadPage(): JSX.Element {
-	return <div>Chessboard</div>;
+	let moves = 0; // doesn't work, must be stateful
+
+	return (
+		<div>
+			Chessboard
+			<button
+				onClick={() =>
+					ipcRenderer
+						.invoke('perform-action', 'Test')
+						.then((result) => {
+							console.log(result);
+							moves = result.cntMovesTotal;
+						})
+				}
+				data-tclass="btn"
+				type="button"
+			>
+				<i className="fa fa-plus" />
+			</button>
+			{moves}
+		</div>
+	);
 }
